d6458a7b8cdb7e67a7052c23ae0f87c5ea97dda3
sanji <oscar.aviglez@gmail.com>
2022-09-15 13:45:12 -0500

    introduccion a API

--- generation-ch16/7-Spring/EjemploMVC/src/main/java/com/generation/oaag/controllers/UsuarioController.java
+++ generation-ch16/7-Spring/EjemploMVC/src/main/java/com/generation/oaag/controllers/UsuarioController.java
@@ -1,17 +1,25 @@
 package com.generation.oaag.controllers;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.generation.oaag.models.UsuarioModel;
 import com.generation.oaag.services.UsuarioServices;
 
 @RestController
 @RequestMapping("/usuario")
+@CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE,
+        RequestMethod.PUT })
 
 
 public class UsuarioController {
@@ -25,6 +33,26 @@ public class UsuarioController {
 	public UsuarioModel guardarUsuario(@RequestBody UsuarioModel usuario){
         return this.usuarioServices.guardarUsuario(usuario);
     }
+	
+	@GetMapping( path = "/{id}")
+	public Optional<UsuarioModel> obtenerUsuarioPorId(@PathVariable("id") Long id) {
+        return this.usuarioServices.obtenerPorId(id);
+    }
+	
+	@GetMapping("/query")
+	public ArrayList<UsuarioModel> obtenerUsuarioPorPrioridad(@RequestParam("prioridad") Integer prioridad){
+        return this.usuarioServices.obtenerPorPrioridad(prioridad);
+    }
+	@DeleteMapping( path = "/{id}")
+	public String eliminarPorId(@PathVariable("id") Long id){
+        boolean ok = this.usuarioServices.eliminarUsuario(id);
+        if (ok){
+            return "Se eliminó el usuario con id " + id;
+        }else{
+            return "No pudo eliminar el usuario con id" + id;
+        }
+    }
+
 
 
 }
--- generation-ch16/7-Spring/EjemploMVC/src/main/java/com/generation/oaag/services/UsuarioServices.java
+++ generation-ch16/7-Spring/EjemploMVC/src/main/java/com/generation/oaag/services/UsuarioServices.java
@@ -1,11 +1,9 @@
 package com.generation.oaag.services;
-
 import org.springframework.stereotype.Service;
-
 import com.generation.oaag.models.UsuarioModel;
 import com.generation.oaag.repositories.UsuarioRepository;
-
 import java.util.ArrayList;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -24,6 +22,20 @@ public class UsuarioServices {
 	public UsuarioModel guardarUsuario(UsuarioModel usuario){
         return usuarioRepository.save(usuario);
     }
+	public Optional<UsuarioModel> obtenerPorId(Long id){
+        return usuarioRepository.findById(id);
+    }
+	public ArrayList<UsuarioModel>  obtenerPorPrioridad(Integer prioridad) {
+        return usuarioRepository.findByPrioridad(prioridad);
+    }
 
+	public boolean eliminarUsuario(Long id) {
+        try{
+            usuarioRepository.deleteById(id);
+            return true;
+        }catch(Exception err){
+            return false;
+        }
+    }
 
 }
